db67544f7354512a34f0997ef7c9ec4c5225d82e
cosmolejo <almego95@gmail.com>
2022-06-14 18:25:29 -0500

    general layout

--- components/sidebar.py
+++ components/sidebar.py
@@ -25,33 +25,3 @@ sidebar = html.Div(
     id="sidebar",
     style=SIDEBAR_STYLE,
 )
-
-
-@app.callback(
-    [
-        Output("sidebar", "style"),
-        Output("page-content", "style"),
-        Output("side_click", "data"),
-    ],
-
-    [Input("btn_sidebar", "n_clicks")],
-    [
-        State("side_click", "data"),
-    ]
-)
-def toggle_sidebar(n, nclick):
-    if n:
-        if nclick == "SHOW":
-            sidebar_style = SIDEBAR_HIDEN
-            content_style = CONTENT_STYLE1
-            cur_nclick = "HIDDEN"
-        else:
-            sidebar_style = SIDEBAR_STYLE
-            content_style = CONTENT_STYLE
-            cur_nclick = "SHOW"
-    else:
-        sidebar_style = SIDEBAR_STYLE
-        content_style = CONTENT_STYLE
-        cur_nclick = 'SHOW'
-
-    return sidebar_style, content_style, cur_nclick
--- main.py
+++ main.py
@@ -15,7 +15,34 @@ app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP],
 suppress_callback_exceptions=True, 
 title='ds4a')
 
+@app.callback(
+    [
+        Output("sidebar", "style"),
+        Output("page-content", "style"),
+        Output("side_click", "data"),
+    ],
 
+    [Input("btn_sidebar", "n_clicks")],
+    [
+        State("side_click", "data"),
+    ]
+)
+def toggle_sidebar(n, nclick):
+    if n:
+        if nclick == "SHOW":
+            sidebar_style = SIDEBAR_HIDEN
+            content_style = CONTENT_STYLE1
+            cur_nclick = "HIDDEN"
+        else:
+            sidebar_style = SIDEBAR_STYLE
+            content_style = CONTENT_STYLE
+            cur_nclick = "SHOW"
+    else:
+        sidebar_style = SIDEBAR_STYLE
+        content_style = CONTENT_STYLE
+        cur_nclick = 'SHOW'
+
+    return sidebar_style, content_style, cur_nclick
 
 
 content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
