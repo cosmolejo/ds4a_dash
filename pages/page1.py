4643bd32c851604b4c020cef647d22269c6efeee
cosmolejo <almego95@gmail.com>
2022-07-03 18:05:27 -0500

    nal graphs

--- pages/page1.py
+++ pages/page1.py
@@ -14,92 +14,91 @@ import json
 #################################################
 
 
-df_Nacional = pd.read_csv('data/Nacional_final.csv')
-df_Nacional = df_Nacional.rename(columns={"Cuenta Heridos":"Cuenta heridos","Cuenta Muertos":"Cuenta muertos","Ciudad":"Departamento"})
-df_Poblacion = pd.read_csv('data/poblacion_departamentos.csv',sep=';')
-
-base = df_Nacional.groupby(["Fecha","Departamento"])["Heridos"].count().reset_index().rename(columns={"Heridos":"Accidentes"})
-base["Fecha"]=pd.to_datetime(base['Fecha'])
-base["Year"]=base["Fecha"].dt.year
-base["Month"]=base["Fecha"].dt.month
-base["DOW"]=base["Fecha"].dt.dayofweek+1
-
-accidentes_por_year = base.groupby(["Departamento","Year"]).sum().drop(columns=["Month","DOW"]).reset_index()
-accidentes_por_year = accidentes_por_year.merge(df_Poblacion[["Departamento","Habitantes"]],how='left',on="Departamento")
-accidentes_por_year["AccidentesNormalizado"]=accidentes_por_year["Accidentes"]/accidentes_por_year["Habitantes"]*1e6
-
-accidentes_total = accidentes_por_year.groupby(["Departamento","Habitantes"]).sum().drop(columns=["Year"]).reset_index()
+map_df = geopandas.read_file('data/nacional/accidentes.geojson')
+accidentes_por_year = pd.read_csv("data/nacional/accidentes_por_year.csv")
+accidentes_total = pd.read_csv("data/nacional/accidentes_total.csv")
+accidentes_por_dia = pd.read_csv("data/nacional/accidentes_por_dia.csv")
+accidentes_por_dia_completo = pd.read_csv(
+    "data/nacional/accidentes_por_dia_completo.csv")
 
 
-accidentes_por_dia = base.groupby(["Departamento","Year","DOW"])["Accidentes"].sum().reset_index()
-accidentes_por_dia = accidentes_por_dia.merge(df_Poblacion[["Departamento","Habitantes"]],how='left',on="Departamento")
-accidentes_por_dia["AccidentesNormalizado"]=accidentes_por_dia["Accidentes"]/accidentes_por_dia["Habitantes"]*1e6
-accidentes_por_dia = accidentes_por_dia.sort_values(["Year","Departamento"])
-
-myshpfile = geopandas.read_file('data/depto.shp')
-myshpfile.set_index('DPTO', drop=True, inplace=True)
-myshpfile = myshpfile.rename(columns={"NOMBRE_DPT":"Departamento"})
-dict = {"ANTIOQUIA": "Antioquia", 
-        "ATLANTICO": "Atlántico",
-        "SANTAFE DE BOGOTA D.C":"Bogotá, D.C.",
-        "BOLIVAR":"Bolívar",
-        "BOYACA":"Boyacá",
-        "CALDAS":"Caldas",
-        "CAQUETA":"Caquetá",
-        "CAUCA":"Cauca",
-        "CESAR":"Cesar",
-        "CORDOBA":"Córdoba",
-        "CUNDINAMARCA":"Cundinamarca",
-        "CHOCO":"Chocó",
-        "HUILA":"Huila",
-        "LA GUAJIRA":"Guajira",
-        "MAGDALENA":"Magdalena",
-        "META":"Meta",
-        "NARIÑO":"Nariño",
-        "NORTE DE SANTANDER":"Norte de Santander",
-        "QUINDIO":"Quindío",
-        "RISARALDA":"Risaralda",
-        "SANTANDER":"Santander",
-        "SUCRE":"Sucre",
-        "TOLIMA":"Tolima",
-        "VALLE DEL CAUCA":"Valle del Cauca",
-        "ARAUCA":"Arauca",
-        "CASANARE":"Casanare",
-        "PUTUMAYO":"Putumayo",
-        "AMAZONAS":"Amazonas",
-        "GUAINIA":"Guainia",
-        "GUAVIARE":"Guaviare",
-        "VAUPES":"Vaupes",
-        "VICHADA":"Vichada",
-        "ARCHIPIELAGO DE SAN ANDRES PROVIDENCIA Y SANTA CATALINA":"San Andres y Providencia",
-    }
-
-myshpfile = myshpfile.replace({"Departamento": dict})
-myshpfile = myshpfile.merge(accidentes_total[["Departamento","Accidentes","AccidentesNormalizado"]],how='left',on="Departamento")
-myshpfile['Accidentes'] = myshpfile['Accidentes'].fillna(0)
-myshpfile['AccidentesNormalizado'] = myshpfile['AccidentesNormalizado'].fillna(0)
-myshpfile.set_index('Departamento', inplace=True)
-
-map_df = myshpfile
-map_df.to_crs(pyproj.CRS.from_epsg(4326), inplace=True)
-
-
-
-
-Map_Fig =  px.choropleth(map_df, geojson=map_df.geometry,locations=map_df.index, color="Accidentes")
+Map_Fig = px.choropleth(map_df, geojson=map_df.geometry,
+                        locations=map_df.index, color="Accidentes")
 Map_Fig.update_geos(fitbounds="locations", visible=True)
-Map_Fig.update_layout(title="National highway's accidents", paper_bgcolor="#F8F9F9")
+Map_Fig.update_layout(title="National highway's accidents",
+                      paper_bgcolor="#F8F9F9")
+
+
+Map_Fig_norm = px.choropleth(map_df, geojson=map_df.geometry,
+                             locations=map_df.index, color="AccidentesNormalizado")
+Map_Fig_norm.update_geos(fitbounds="locations", visible=True)
+Map_Fig_norm.update_layout(title="Normalized National highway's accidents",
+                           paper_bgcolor="#F8F9F9")
+
+bar_plot_nal = px.bar(accidentes_total, x="Departamento", y="AccidentesNormalizado",
+                      title="Accidentes por millón de habitantes", barmode='group')
+bar_plot_nal.update_layout(paper_bgcolor="#F8F9F9")
+
+year_bar_plot_nal = []
+for year in accidentes_por_year["Year"].unique():
+    year_bar_plot_nal += [px.bar(accidentes_por_year[accidentes_por_year["Year"] == year], x="Departamento",
+                                 y="AccidentesNormalizado", title="Accidentes año: "+str(year)+", \
+                                (normalizado por millon de habitantes)")]
+
+
+bar_plot_accidents = px.bar(accidentes_por_year, x="Year", y="AccidentesNormalizado",
+                            title="Accidentes por año por millón de habitantes", color="Departamento", barmode='group')
+bar_plot_accidents.update_layout(title="yearly accidents",
+                                 paper_bgcolor="#F8F9F9")
+bar_plot_accidents_w = px.bar(accidentes_por_dia_completo, x="DOW", y="AccidentesNormalizado",
+                              title="Accidentes por dia de la semana (por Millón de habitantes)", color="Departamento", barmode='group')
+bar_plot_accidents_w.update_layout(title="weekly accidents",
+                                   paper_bgcolor="#F8F9F9")
+
+
+year_accidets_day = []
+for year in accidentes_por_dia["Year"].unique():
+    year_accidets_day += [px.bar(accidentes_por_dia[accidentes_por_dia["Year"] == year], x="DOW", y="AccidentesNormalizado",
+                                 title="Accidentes por dia de la semana (por Millón de habitantes) para el año "+str(year),
+                                  color="Departamento", barmode='group')]
 
 
 layout = html.Div([
-    #html.H3('Page 1'),
-    #html.P('here goes the national plots')
+    # html.H3('Page 1'),
+    # html.P('here goes the national plots')
     html.Div(
-    [
-        # Place the main graph component here:
-        dcc.Graph(figure=Map_Fig, id="US_map")
-    ]
-)
+        [
+            # Place the main graph component here:
+            dcc.Graph(figure=Map_Fig, id="US_map")
+        ]),
+    html.Div(
+        [
+            # Place the main graph component here:
+            dcc.Graph(figure=Map_Fig_norm, id="US_map")
+        ]),
+    html.Div(
+        [
+            # Place the main graph component here:
+            dcc.Graph(figure=bar_plot_nal, id="US_map")
+        ]),
+    html.Div(
+        [
+            dcc.Graph(figure=i, id="US_map") for i in year_bar_plot_nal
+        ]),
+    html.Div(
+        [
+            # Place the main graph component here:
+            dcc.Graph(figure=bar_plot_accidents, id="US_map")
+        ]),
+    html.Div(
+        [
+            # Place the main graph component here:
+            dcc.Graph(figure=bar_plot_accidents_w, id="US_map")
+        ]),
+    html.Div(
+        [
+            dcc.Graph(figure=i, id="US_map") for i in year_accidets_day
+        ])
 ])
 
 
@@ -135,9 +134,7 @@ def page1_callbacks(app):
     #     )
     #     return fig_map2
 
-
     # MAP click interaction
-
 
     # @app.callback(
     #     Output("state_dropdown", "value"),
